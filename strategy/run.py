2344d8b45a166e43ce48b5698fb53737486ae6d4
joffrey <origingod.oh@gmail.com>
2025-03-04 10:25:05 +0800

    U: base remove unused code

--- strategy/run.py
+++ strategy/run.py
@@ -55,36 +55,9 @@ def init_engine():
     return engine
 
 def main():
-    #engine.connect_gateway(binance_setting, "BINANCE")
-    #engine.connect_gateway(huobi_setting, "HUOBIS")
-    #engine.connect_gateway(huobi_setting, "HUOBI")
-    engine = init_engine()
-    #log_engine = engine.main_engine.get_engine('log')
-    #engine.event_engine.register(EVENT_CRYPTO_LOG, log_engine.process_log_event)
-    # engine.event_engine.register(EVENT_ACCOUNT, log_engine.process_log_event)
-    # cta_engine = engine.main_engine.add_app(CtaStrategyApp)
-    # time.sleep(3)
-    # cta_engine.init_engine()
-    # engine.write_log("CTA引擎初始化完成")
-    # time.sleep(5)
-    # cta_engine.init_all_strategies()
-    # 
-    # while True:
-    #     inited = [(k,v) for k,v in cta_engine.strategies.items() if v.inited==True]
-    #     all_strategies = [(k,v) for k,v in cta_engine.strategies.items()]
-    #     engine.write_log(f"已加载: {len(inited)}/{len(all_strategies)}")
-    #     if len(inited) == len(all_strategies):
-    #         engine.write_log("CTA策略加载完成")
-    #         break
-    #     else:
-    #         time.sleep(3)
-    # 
-    # cta_engine.start_all_strategies()
-    # engine.write_log("CTA启动完成")
 
-    #recorder_engine = engine.main_engine.get_engine(DataRecorderApp.app_name)
-    #recorder_engine.add_bar_recording('btcusdt.BINANCE')
-    #recorder_engine.add_tick_recording('btcusdt.BINANCE')
+    engine = init_engine()
+
 
     time.sleep(1)
     engine.start_strategy('demo.py')
